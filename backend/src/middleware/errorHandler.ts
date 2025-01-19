bc392a2d1bfdef56dcd7029df73846d7d2cf400a
Tony Baidoo <tony.baidoo@hubtel.com>
2025-01-19 13:28:27 +0000

    clear authentication cookies on refresh path in error handler

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,9 +2,15 @@ import { ErrorRequestHandler } from "express";
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
 import { z } from "zod";
 import AppError from "../utils/appError";
+import { REFRESH_PATH } from "../constants/constants";
+import { clearAuthCookies } from "../utils/utils";
 
 export const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log(`Path: ${req.path}`, err);
+
+  if(req.path === REFRESH_PATH) {
+    clearAuthCookies(res)
+  }
 
   if (err instanceof z.ZodError) {
     return res.status(BAD_REQUEST).json({
